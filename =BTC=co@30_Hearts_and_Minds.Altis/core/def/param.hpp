33a5843b33b800a3f2ba9da6e01062a09e5af6d8
mrschick <58027418+mrschick@users.noreply.github.com>
2026-06-11 23:08:02 +0200

    Abilita Droni Bombaroli

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -285,7 +285,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_IED_DRONE"]);
         values[]={0, 1};
         texts[]={$STR_DISABLED,$STR_ENABLED};
-        default = 0;
+        default = 1;
     };
     class btc_p_hideout_cache_title { // << Hideout/Cache options >>
         title = $STR_BTC_HAM_PARAM_HIDE_TITLE;
